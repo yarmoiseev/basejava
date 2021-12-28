e64b0f43c1b8f7a9c6013a2b34b73f8d8cad2b3a
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2021-12-28 16:05:05 +0300

    HW7: bugfix #1 and branching test

--- src/com/yarmoiseev/webapp/ResumeTestData.java
+++ src/com/yarmoiseev/webapp/ResumeTestData.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResumeTestData {
+
     public static void main(String[] args) {
 
         List<String> achievements = new ArrayList<>();
