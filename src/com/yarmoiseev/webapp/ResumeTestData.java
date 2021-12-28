6d48d6c9de9c0c3ae400e19f300fbfb4d6099ed6
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2021-12-28 15:57:38 +0300

    HW7: git test

--- src/com/yarmoiseev/webapp/ResumeTestData.java
+++ src/com/yarmoiseev/webapp/ResumeTestData.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResumeTestData {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Todo some shit
 
         List<String> achievements = new ArrayList<>();
 
