6672586ec68a7a709f7987882b961cea9ff700f2
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2022-02-07 11:30:05 +0300

    HW11: deadlock demo bugfix#2

--- src/com/yarmoiseev/webapp/MainDeadlockDemo.java
+++ src/com/yarmoiseev/webapp/MainDeadlockDemo.java
@@ -5,35 +5,8 @@ public class MainDeadlockDemo {
     static final String resource2 = "resource2";
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            synchronized (resource1) {
-                //System.out.println("Thread 1 locked resource 1");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                synchronized (resource2) {
-                    //System.out.println("Thread 1 locked resource 2");
-                }
-            }
-        });
-
-        Thread t2 = new Thread(() -> {
-            synchronized (resource2) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //System.out.println("Thread 2 locked resource 2");
-
-                synchronized (resource1) {
-                    //System.out.println("Thread 2 locked resource 1");
-                }
-            }
-        });
+        Thread t1 = makeThread(resource1, resource2);
+        Thread t2 = makeThread(resource2, resource1);
         startThreads(t1, t2);
 
     }
@@ -41,6 +14,23 @@ public class MainDeadlockDemo {
     static void startThreads(Thread t1, Thread t2) {
         t1.start();
         t2.start();
+    }
+
+    static Thread makeThread(String r1, String r2) {
+        return new Thread(() -> {
+            synchronized (r1) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (r2) {
+
+                }
+            }
+        });
     }
 
 }
