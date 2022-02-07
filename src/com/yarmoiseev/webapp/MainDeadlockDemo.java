4151ee40a4c380d6536e94c40c2ba7b282e8a190
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2022-02-07 15:40:06 +0300

    HW11: deadlock demo bugfix#3

--- src/com/yarmoiseev/webapp/MainDeadlockDemo.java
+++ src/com/yarmoiseev/webapp/MainDeadlockDemo.java
@@ -5,21 +5,16 @@ public class MainDeadlockDemo {
     static final String resource2 = "resource2";
 
     public static void main(String[] args) {
-        Thread t1 = makeThread(resource1, resource2);
-        Thread t2 = makeThread(resource2, resource1);
-        startThreads(t1, t2);
+        makeThread(resource1, resource2).start();
+        makeThread(resource2, resource1).start();
 
     }
 
-    static void startThreads(Thread t1, Thread t2) {
-        t1.start();
-        t2.start();
-    }
 
     static Thread makeThread(String r1, String r2) {
         return new Thread(() -> {
             synchronized (r1) {
-
+                System.out.println("Thread " + Thread.currentThread().getName() + " took " + r1);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
