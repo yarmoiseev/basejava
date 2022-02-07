2711c3ca2c8aa1197896be849d021b284cd516ad
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2022-02-07 10:34:42 +0300

    HW11: deadlock demo bugfix#1

--- src/com/yarmoiseev/webapp/MainDeadlockDemo.java
+++ src/com/yarmoiseev/webapp/MainDeadlockDemo.java
@@ -7,24 +7,38 @@ public class MainDeadlockDemo {
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             synchronized (resource1) {
-                System.out.println("Thread 1 locked resource 1");
+                //System.out.println("Thread 1 locked resource 1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
                 synchronized (resource2) {
-                    System.out.println("Thread 1 locked resource 2");
+                    //System.out.println("Thread 1 locked resource 2");
                 }
             }
         });
 
         Thread t2 = new Thread(() -> {
             synchronized (resource2) {
-                System.out.println("Thread 2 locked resource 2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //System.out.println("Thread 2 locked resource 2");
 
                 synchronized (resource1) {
-                    System.out.println("Thread 2 locked resource 1");
+                    //System.out.println("Thread 2 locked resource 1");
                 }
             }
         });
+        startThreads(t1, t2);
 
+    }
+
+    static void startThreads(Thread t1, Thread t2) {
         t1.start();
         t2.start();
     }
