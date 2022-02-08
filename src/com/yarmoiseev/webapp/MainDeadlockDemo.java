fe1017ce100aecb8c0ea82c2304bd1235849f4ed
Yaroslav Moiseev <yarmoiseev01@gmail.com>
2022-02-08 17:53:53 +0300

    HW11: deadlock demo bugfix#4

--- src/com/yarmoiseev/webapp/MainDeadlockDemo.java
+++ src/com/yarmoiseev/webapp/MainDeadlockDemo.java
@@ -14,13 +14,13 @@ public class MainDeadlockDemo {
     static Thread makeThread(String r1, String r2) {
         return new Thread(() -> {
             synchronized (r1) {
-                System.out.println("Thread " + Thread.currentThread().getName() + " took " + r1);
+                System.out.println("Thread " + Thread.currentThread().getName() + " took  " + r1);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
+                System.out.println("Thread " + Thread.currentThread().getName() + " is trying to take  " + r2);
                 synchronized (r2) {
 
                 }
