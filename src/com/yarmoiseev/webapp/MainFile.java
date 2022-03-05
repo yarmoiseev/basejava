ac289ec481a35c5191248f383c104b28fe7aefeb
yarmoiseev <yarmoiseev01@gmail.com>
2022-03-05 12:39:24 +0300

    New PC test commit

--- src/com/yarmoiseev/webapp/MainFile.java
+++ src/com/yarmoiseev/webapp/MainFile.java
@@ -5,6 +5,8 @@ import java.io.File;
 
 public class MainFile {
 
+    //Teeeeeest
+
 
     public static void main(String[] args) {
         final File folder = new File(".\\src");
---
+++ test/com/yarmoiseev/webapp/storage/JustTestClass.java
@@ -0,0 +1,11 @@
+package com.yarmoiseev.webapp.storage;
+
+public class JustTestClass {
+    public static void main(String[] args) {
+        doNothing();
+    }
+
+    public static void doNothing() {
+
+    }
+}
